27a0ea379965f882e7d0609ccc5744fe7d69c75d
Esther Pato <estherpato@gmail.com>
2018-07-27 17:34:04 +0200

    It does not work but welp

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React, { Component, Fragment } from 'react';
 import './App.css';
-import PokeList from './components/List.js';
 
 class App extends Component {
   render() {
+    const array = this.props.pokemonList
     return (
       <Fragment>
-      <h1>Mi lista de Pokemon</h1>
-      <PokeList />
+        <h1>Mi lista de Pokemon</h1>
+        <ul>
+          {array.map(function (item) {
+            return (
+              <li>
+                id: {item.id}
+                name: {item.name}
+                types: {item.types}
+                evolution: {item.evolution}
+                url: {item.url}
+              </li>
+            )
+          })}
+        </ul>
       </Fragment>
     );
   }
