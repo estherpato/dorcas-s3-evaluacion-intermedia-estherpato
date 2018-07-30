0e1365b954b541868ed052ccfc2d43cd848dc346
Esther Pato <estherpato@gmail.com>
2018-07-31 00:51:58 +0200

    Add key on card li and functioning set state

--- src/App.js
+++ src/App.js
@@ -5,41 +5,35 @@ import PokemonList from './components/PokemonList';
 class App extends Component {
   constructor() {
     super();
+
     this.handleClick = this.handleClick.bind(this);
-    this.bgnormal = 'PokemonCard__container'
-    this.bgclicked = 'PokemonCard__container--click'
+
+    this.state = {
+      background: 'PokemonCard__container'
+    }   
 }
 
   //callback
-  handleClick(event) {
-    
-    if(event.target) {
-      this.bgnormal = this.bgclicked;
-      console.log('funciono')
-    } else {
-      this.bgnormal;
-    }
-    this.forceUpdate();
+  handleClick() {
+    this.setState(
+      {background: 'PokemonCard__container--click'}
+    )
   }
 
   render() {
     const { pokemonList } = this.props
     return (
       <Fragment>
-<<<<<<< HEAD
-        <h1 className="App-title">my pokemon list</h1>
-        <PokemonList 
-        pokemon={pokemonList}
-        callback={this.handleClick}
-        backgroundColor={this.bgnormal} />
-=======
         <header>
           <h1 className="App-title">my pokemon list</h1>
         </header>
         <main>
-          <PokemonList pokemon={pokemonList} />
+          <PokemonList 
+          pokemon={pokemonList} 
+          backgroundClass={this.state.background}
+          callback={this.handleClick}
+          />
         </main>
->>>>>>> master
       </Fragment>
     );
   }
--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -4,16 +4,19 @@ import './PokemonList.css';
 
 class PokemonList extends Component {
     render() {
-        const { pokemon, backgroundColor, callback } = this.props
+        const { pokemon, backgroundClass, callback } = this.props
         return (
             <ul className="PokemonList-wrapper">
                 {pokemon.map(pokemon =>
-                    <li onClick={callback}>
+                    <li
+                        onClick={callback}
+                        key={pokemon.id}
+                    >
                         <PokemonCard
                             picURL={pokemon.url}
                             name={pokemon.name}
                             types={pokemon.types}
-                            cardBackground={backgroundColor}
+                            cardBackground={backgroundClass}
                         />
                     </li>)}
             </ul>
